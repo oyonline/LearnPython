8c3977c423804a803977546530029a8e33b185de
Ouyang ding <81409107@qq.com>
2025-04-03 11:15:53 +0800

    Revert "参数化查询规范"

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import update_task
 
 
 #执行更新操作
-configtest.update_account_balance()
+#configtest.update_account_balance()
 
 
 # 执行删除函数
@@ -19,4 +19,4 @@ configtest.update_account_balance()
 
 
 #create_new_task.create_new_task()
-#update_task.update_task()
+update_task.update_task()
